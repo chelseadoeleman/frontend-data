7d9f50106782c3a0a4777d30072e294bc8759749
Chelsea <Chelsea.Doeleman@hva.nl>
2018-11-13 14:46:01 +0100

    get desired results with helpers

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -1,21 +1,20 @@
 // Shout out naar Maikel voor het helpen
 
-const _range = require ("lodash.range")
-const fs = require("fs")
+// const _range = require ("lodash.range")
+// const fs = require("fs")
 
 // Checks to make sure the results are there in the data and will not unexpectedly break.
-const getAuthorFromResult = (result) => {
-    return result.authors
-        && result.authors["main-author"]
-        && result.authors["main-author"].$t
+const getTitleFromResult = (result) => {
+    return result.titles
+        && result.titles["short-title"]
+        && result.titles["short-title"].$t
         || undefined
 }
 
-const getPublisherFromResult = (result) => {
-    return result.publication
-        && result.publication.publishers
-        && result.publication.publishers.publisher
-        && result.publication.publishers.publisher.$t
+const getDurationFromResult = (result) => {
+    return result.description
+        && result.description["physical-description"]
+        && result.description["physical-description"].$t
         || undefined
 }
 
@@ -27,86 +26,108 @@ const getPublicationYearFromResult = (result) => {
         || undefined
 }
 
-const getLanguageFromResult = (result) => {
-    return result.languages
-        && result.languages.language
-        && result.languages.language.$t
-        || undefined
-}
-
-// getPublicationYears from the last 30 years.
-const getPublicationYears = () => {
-    const currentYear = new Date().getFullYear()
-    // Currentyear + 1 because else it wouldn't count past 1 januari 2018
-    const publicationYears = _range(currentYear - 30, currentYear + 1)
-
-    return publicationYears
-}
-
-// Shout out naar Wouter 
-const getFilterdGender = {};
-
-// Check if the name is male of female or maybe can't even be identified
-const getGenderFromName = (firstname) => {
-	if (Object.keys(getFilterdGender ).length <= 0) {
-		Object.assign(getFilterdGender , JSON.parse(fs.readFileSync(__dirname + "/../data/names.json", "utf8")));
-	}
-	const man = getFilterdGender .mannen.find(name => name === firstname);
-    const vrouw = getFilterdGender .vrouwen.find(name => name === firstname);
-	if (!(man || vrouw) || man && vrouw) return null;
-	return (man && "Man") || (vrouw && "Vrouw");
-}
-
-// return Name and Gender in a new object.
-const getNameAndGender = (author) => {
-    const authorFirstNameLastName = author && author.split(", ")
-    let firstName = authorFirstNameLastName && authorFirstNameLastName.length && authorFirstNameLastName[1]
-    const firstDot = firstName && firstName.indexOf(".")
-    // When there are no dots return "falsy" so it won't use the following function: getTransformedFirstName
-    const hasDots = firstDot !== -1
-    // Remove tokens from firstname with parameters to use in the that function
-    const transformedFirstName = getTransformedFirstName(hasDots, firstName, firstDot)
-    // If there are dots return transformedFirstname else just the firstName 
-    // Add gender to that equation
-    const nameToUse = hasDots ? transformedFirstName : firstName
-    return {
-        name: nameToUse,
-        // call the following function, to see what gender belongs to that name
-        gender: getGenderFromName(nameToUse),
+const getGenreFromResult = (result) => {
+    const genres = result.genres
+    if (genres) {
+        if (genres && genres.genre) {
+            const genre = genres.genre
+            if (Array.isArray(genre)) {
+                return result.genres.genre.map(genre => genre.$t)
+            } else {
+                return result.genres.genre.$t
+            }
+        } else {
+            return undefined
+        }
+    } else {
+        return undefined
     }
 }
 
-// Remove tokens from firstname 
-const getTransformedFirstName = (hasDots, firstName, firstDot) => {
-    // One step back from the first dot
-    const removeStartIndex = hasDots ? firstDot - 1 : undefined
-    const endIndex = firstName && firstName.length
-    // Remove the tokens and check if the firstName = not undefined
-    const tokensToRemove = firstName && removeStartIndex !== undefined && firstName.slice(removeStartIndex, endIndex)
-    // Remove space from firstName after removing the tokens
-    const transformedFirstName = tokensToRemove && firstName.replace(tokensToRemove, '').trim()
-    return transformedFirstName
+const getCoverImageFromResult = (result) => {
+    return result.coverimages
+        && result.coverimages.coverimage
+        && Array.isArray(result.coverimages.coverimage)
+            ? result.coverimages.coverimage[0].$t
+            : result.coverimages.coverimage.$t || undefined
 }
+
+// // getPublicationYears from the last 30 years.
+// const getPublicationYears = () => {
+//     const currentYear = new Date().getFullYear()
+//     // Currentyear + 1 because else it wouldn't count past 1 januari 2018
+//     const publicationYears = _range(currentYear - 200, currentYear + 1)
+
+//     return publicationYears
+// }
+
+// // Shout out naar Wouter 
+// const getFilterdGender = {};
+
+// // Check if the name is male of female or maybe can't even be identified
+// const getGenderFromName = (firstname) => {
+// 	if (Object.keys(getFilterdGender ).length <= 0) {
+// 		Object.assign(getFilterdGender , JSON.parse(fs.readFileSync(__dirname + "/../data/names.json", "utf8")));
+// 	}
+// 	const man = getFilterdGender .mannen.find(name => name === firstname);
+//     const vrouw = getFilterdGender .vrouwen.find(name => name === firstname);
+// 	if (!(man || vrouw) || man && vrouw) return null;
+// 	return (man && "Man") || (vrouw && "Vrouw");
+// }
+
+// // return Name and Gender in a new object.
+// const getNameAndGender = (author) => {
+//     const authorFirstNameLastName = author && author.split(", ")
+//     let firstName = authorFirstNameLastName && authorFirstNameLastName.length && authorFirstNameLastName[1]
+//     const firstDot = firstName && firstName.indexOf(".")
+//     // When there are no dots return "falsy" so it won't use the following function: getTransformedFirstName
+//     const hasDots = firstDot !== -1
+//     // Remove tokens from firstname with parameters to use in the that function
+//     const transformedFirstName = getTransformedFirstName(hasDots, firstName, firstDot)
+//     // If there are dots return transformedFirstname else just the firstName 
+//     // Add gender to that equation
+//     const nameToUse = hasDots ? transformedFirstName : firstName
+//     return {
+//         name: nameToUse,
+//         // call the following function, to see what gender belongs to that name
+//         gender: getGenderFromName(nameToUse),
+//     }
+// }
+
+// // Remove tokens from firstname 
+// const getTransformedFirstName = (hasDots, firstName, firstDot) => {
+//     // One step back from the first dot
+//     const removeStartIndex = hasDots ? firstDot - 1 : undefined
+//     const endIndex = firstName && firstName.length
+//     // Remove the tokens and check if the firstName = not undefined
+//     const tokensToRemove = firstName && removeStartIndex !== undefined && firstName.slice(removeStartIndex, endIndex)
+//     // Remove space from firstName after removing the tokens
+//     const transformedFirstName = tokensToRemove && firstName.replace(tokensToRemove, '').trim()
+//     return transformedFirstName
+// }
 
 // Return new object while mapping over all results to get only the specific result for research
 const getTransformedResultFromResults = (results) => {
     return results 
         ? results.map(result => ({
-            author: getAuthorFromResult(result),
-            publisher: getPublisherFromResult(result),
+            title: getTitleFromResult(result),
+            duration: getDurationFromResult(result),
             publicationYear: getPublicationYearFromResult(result),
-            language: getLanguageFromResult(result)
-        })) 
+            genre: getGenreFromResult(result),
+            coverImage: getCoverImageFromResult(result)
+        }
+        )) 
         : []
 }
 
 // Export the following functions
 module.exports = {
-    getTransformedResultFromResults, 
-    getPublicationYearFromResult, 
-    getGenderFromName, 
-    getNameAndGender,
-    getPublicationYears
+    getTransformedResultFromResults,
+    getGenreFromResult
+    // getPublicationYearFromResult, 
+    // getGenderFromName, 
+    // getNameAndGender,
+    // getPublicationYears
 }
 
 
--- index.js
+++ index.js
@@ -23,21 +23,21 @@ const search = async (q, facet) => {
 		refine: true,
 		facet,
 		// Return amount of results 
-		count: 1500,
-		filter: (result) => {
-			const publicationYear = helpers.getPublicationYearFromResult(result)
-			const currentYear = new Date().getFullYear()
+		count: 20,
+		// filter: (result) => {
+		// 	// const publicationYear = helpers.getPublicationYearFromResult(result)
+		// 	// const currentYear = new Date().getFullYear()
 
-			// filter on publicationYear from the last 30 years
-			return publicationYear >= currentYear - 30
-		}
+		// 	// // filter on publicationYear from the last 30 years
+		// 	// return publicationYear >= currentYear - 200
+		// }
   	})  
 }
 
 // Shout out naar Maikel
 (async () => {
 	try {
-		const results = await search("language:eng", "type(book)")
+		const results = await search("language:eng", "type(movie)")
 		// ("harry potter", ["type(book)", "auteur(J.K. Rowling)"])
 		// meerdere facetten toevoegen thanks to Jessie
 
@@ -45,54 +45,58 @@ const search = async (q, facet) => {
 			// Return a new object with specific results
 			const transformedResults = helpers.getTransformedResultFromResults(results)
 			// get all the authors from transformedResults
-			const authors = transformedResults.map(result => result.author)
+			const title = transformedResults.map(result => result.title)
 			// Map over authors to get the gender of every author
-			const transformedAuthors = authors.map(helpers.getNameAndGender)
+			// const transformedAuthors = authors.map(helpers.getNameAndGender)
 			
-			// Create new a new array with all the author names and belonging gender
-			const filterTransformedAuthors = transformedAuthors.filter(transformedAuthor => {
-				return transformedAuthor.name && transformedAuthor.gender
-			})
+			// // Create new a new array with all the author names and belonging gender
+			// const filterTransformedAuthors = transformedAuthors.filter(transformedAuthor => {
+			// 	return transformedAuthor.name && transformedAuthor.gender
+			// })
 
-			// Get the gender from every year by mapping over getPublicationYears in a specific range
-			const getGenderFromYear = helpers.getPublicationYears().map(year => {
-				// Get the results for every specific year within that range
-				const resultsByYear = transformedResults.filter(result => result.publicationYear === year) 
-				// Map over results from resultsByYear to get the specific author and create an new array
-				// with the author and gender
-				const gendersAndNames =	resultsByYear
-					.map(result => helpers.getNameAndGender(result.author))
-					.filter(result => result.name && result.gender)
+			// // Get the gender from every year by mapping over getPublicationYears in a specific range
+			// const getGenderFromYear = helpers.getPublicationYears().map(year => {
+			// 	// Get the results for every specific year within that range
+			// 	const resultsByYear = transformedResults.filter(result => result.publicationYear === year) 
+			// 	// Map over results from resultsByYear to get the specific author and create an new array
+			// 	// with the author and gender
+			// 	const gendersAndNames =	resultsByYear
+			// 		.map(result => helpers.getNameAndGender(result.author))
+			// 		.filter(result => result.name && result.gender)
 				
-				// Get the amount of genders in that specific year
-				const amountOfMen = gendersAndNames.filter(genderAndName => genderAndName.gender === "Man").length
-				const amountOfWomen = gendersAndNames.filter(genderAndName => genderAndName.gender === "Vrouw").length
-				// New Object to create a new data structure to use for the visualization
-				return {
-					// Year is specified as an string
-					category: year.toString(),
-					values: [
-						{
-							gender: "men",
-							value: amountOfMen,
-						},
-						{
-							gender: "women",
-							value: amountOfWomen,
-						},
-					]
-				}
-			})
+			// 	// Get the amount of genders in that specific year
+			// 	const amountOfMen = gendersAndNames.filter(genderAndName => genderAndName.gender === "Man").length
+			// 	const amountOfWomen = gendersAndNames.filter(genderAndName => genderAndName.gender === "Vrouw").length
+			// 	// New Object to create a new data structure to use for the visualization
+			// 	return {
+			// 		// Year is specified as an string
+			// 		category: year.toString(),
+			// 		values: [
+			// 			{
+			// 				gender: "men",
+			// 				value: amountOfMen,
+			// 			},
+			// 			{
+			// 				gender: "women",
+			// 				value: amountOfWomen,
+			// 			},
+			// 		]
+			// 	}
+			// })
 
-			console.log(getGenderFromYear)
-			console.log(filterTransformedAuthors)
 			console.log(transformedResults)
 
+			// console.log(getGenderFromYear)
+			// console.log(filterTransformedAuthors)
+			// console.log(transformedResults)
+			// console.log(results)
+
+
 			// Get results in localhost on port 3000 
-			app.get("/", (req, res) => res.json(getGenderFromYear))
+			app.get("/", (req, res) => res.json(transformedResults))
 			app.listen(port, () => console.log(chalk.green(`Listening on port ${port}`)))
 			// Write results to data.json
-			fs.writeFile("data.json", JSON.stringify(getGenderFromYear), (err) => err && console.error(err))
+			// fs.writeFile("data.json", JSON.stringify(getGenderFromYear), (err) => err && console.error(err))
 		}
 	} catch (error) {
 		throw new Error(error)
